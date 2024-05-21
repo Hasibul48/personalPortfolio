591e18008db2f8fa17e2ec17f867d1de8be25827
Hasibul-Islam <hmhasibul48@gmail.com>
2024-05-21 09:28:30 +0600

    Works Section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	// Header Scroll
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		var scroll = $(window).scrollTop();
 
 		if (scroll >= 50) {
@@ -17,36 +17,41 @@ $(document).ready(function() {
 
 	// Page Scroll
 	var sections = $('section')
-		nav = $('nav[role="navigation"]');
+	nav = $('nav[role="navigation"]');
 
 	$(window).on('scroll', function () {
-	  	var cur_pos = $(this).scrollTop();
-	  	sections.each(function() {
-	    	var top = $(this).offset().top - 76
-	        	bottom = top + $(this).outerHeight();
-	    	if (cur_pos >= top && cur_pos <= bottom) {
-	      		nav.find('a').removeClass('active');
-	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-	    	}
-	  	});
+		var cur_pos = $(this).scrollTop();
+		sections.each(function () {
+			var top = $(this).offset().top - 76
+			bottom = top + $(this).outerHeight();
+			if (cur_pos >= top && cur_pos <= bottom) {
+				nav.find('a').removeClass('active');
+				nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+			}
+		});
 	});
 	nav.find('a').on('click', function () {
-	  	var $el = $(this)
-	    	id = $el.attr('href');
+		var $el = $(this)
+		id = $el.attr('href');
 		$('html, body').animate({
 			scrollTop: $(id).offset().top - 75
 		}, 500);
-	  return false;
+		return false;
 	});
 
 	// Mobile Navigation
-	$('.nav-toggle').on('click', function() {
+	$('.nav-toggle').on('click', function () {
 		$(this).toggleClass('close-nav');
 		nav.toggleClass('open');
 		return false;
-	});	
-	nav.find('a').on('click', function() {
+	});
+	nav.find('a').on('click', function () {
 		$('.nav-toggle').toggleClass('close-nav');
 		nav.toggleClass('open');
 	});
 });
+
+// card
+document.querySelector(".myCard").addEventListener("click", () => {
+	window.open("https://beautiful-meerkat-f5530a.netlify.app/")
+})
