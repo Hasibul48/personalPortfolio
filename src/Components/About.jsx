41f0833b96ef1b50285f365c53fe8236e32f429b
Hasibul-Islam <hmhasibul48@gmail.com>
2024-08-06 18:18:45 +0600

    Some Modification

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function About() {
     return (
-        <div className='text-center md:mt-10 mt-28 md:text-start container'>
+        <div className='md:mt-10 mt-28 container'>
             <h2 className='text-center font-handWriting md:text-5xl text-4xl tracking-wider '>About Me</h2>
             <div className="md:flex justify-center items-center">
                 <div className="flex-1 px-7">
-                    <h2 className='text-themeColor font-bold md:text-xl mt-7'>Who is Hasibul?</h2>
-                    <p className='font-extralight tracking-widest'>I am Hasibul a passionate MERN stack web developer with a strong foundation in MongoDB, Express.js, React.js, and Node.js. Although I have no formal work experience, I have completed several personal and academic projects that showcase my skills. I am dedicated to writing clean, maintainable code and continuously learning new technologies. I am eager to collaborate with experienced developers and contribute to impactful projects.</p>
+                    <h2 className='text-themeColor font-extrabold md:text-xl md:text-start mt-7'>Who is Hasibul?</h2>
+                    <p className='font-extralight tracking-widest md:text-start'>I am Hasibul a passionate MERN stack web developer with a strong foundation in MongoDB, Express.js, React.js, and Node.js. Although I have no formal work experience, I have completed several personal and academic projects that showcase my skills. I am dedicated to writing clean, maintainable code and continuously learning new technologies. I am eager to collaborate with experienced developers and contribute to impactful projects.</p>
                 </div>
 
                 <div className="flex-1 px-10 pt-10">
-                    <img className={'headerImg mx-auto lg:w-3/4 shadow-2xl shadow-gray-700 border-4 border-themeColor'} src="img/profileImage2.jpg" alt="image missing..." />
+                    <img className='headerImg mx-auto md:w-3/4 shadow-2xl shadow-gray-700 border-4 border-themeColor' src="img/profileImage2.jpg" alt="image missing..." />
                 </div>
 
             </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
                 <img className={'headerImg mx-auto lg:w-3/4 shadow-2xl shadow-gray-700 border-4 border-themeColor'} src="img/profileImage.jpg" alt="image missing..." />
             </div>
             <div className="flex-1 md:ml-16 md:mt-0 mt-20 md:text-start text-center leading-8 md:leading-10">
-                <h1 className='text-themeColor font-bold md:text-xl'>Hi, I am</h1>
+                <h1 className='text-themeColor font-extrabold md:text-xl'>Hi, I am</h1>
                 <h2 className='font-handWriting md:text-4xl text-3xl tracking-wider'>Hasibul Islam</h2>
                 <p className='font-extralight tracking-widest'>Programmer, Web Developer (MERN), Contributor</p>
             </div>
